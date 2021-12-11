02e63de5ae4d9efe4d53140b64c2b94f72fafcdd
Justin Huang <justin.2260@gmail.com>
2021-12-10 20:04:46 -0500

    refactored code, cleaned code

--- express_server.js
+++ express_server.js
@@ -2,21 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-//const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
 const { reset } = require("nodemon");
 const { generateRandomString, getUserByEmail, urlsForUser } = require("./helpers");
 
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(cookieParser());
 app.use(
   cookieSession({
     name: 'session',
     keys: ["secretKey1", "secretKey2"]
   })
 );
-
 app.set("view engine", "ejs");
 
 //List of sample URLs and users
@@ -44,14 +41,27 @@ const users = {
   }
 };
 
+//Get / page
+app.get("/", (req, res) => {
+  //if user is not logged in then redirect to login page
+  if (!users[req.session.user_id]) {
+    res.redirect("/login");
+  //if they are then redirect them to their list of URL page
+  } else {
+    res.redirect("/urls");
+  }
+});
+
 //Get list of URL page for each specific user
 app.get("/urls", (req, res) => {
+  //using helper function to grab list of user specific URLs
   const listOfUserUrls = urlsForUser(req.session.user_id, urlDatabase);
   const templateVars = {
     urls: listOfUserUrls,
     user: users[req.session.user_id]
   };
 
+  //rendering the HTML page with the help of templateVars
   res.render("urls_index", templateVars);
 });
 
@@ -59,31 +69,36 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const templateVars = { user: users[req.session.user_id] };
 
+  //redirect to login page if user is not logged in
   if (!users[req.session.user_id]) {
     res.redirect("/login");
-    return;
+  } else {
+    //rendering the url/new HTML page with the help of templateVars
+    res.render("urls_new", templateVars);
   }
-
-  res.render("urls_new", templateVars);
 });
 
 //Create new shortURL, adding to urlDatabase
 app.post("/urls", (req, res) => {
   
+  //prevent users who are no logged in to create a new shortURL
   if (!users[req.session.user_id]) {
-    res.status(401).send("Cannot create new URLs when not logged in. Please login!");
-    return;
+    res.status(401).send("<h1>Cannot create new URLs when not logged in. Please login!</h1>");
+  } else {
+    //generate the random ID for the new shortURL
+    const shortURL = generateRandomString();
+    //obtain the URL of the actual site from the submission of the form
+    const longURL = req.body.longURL;
+  
+    //add the new shortURL information to the database
+    urlDatabase[shortURL] = {
+      longURL,
+      userID: req.session.user_id
+    };
+    console.log(urlDatabase);
+    //redirect users to the new shortURL page
+    res.redirect(`/urls/${shortURL}`);
   }
-  
-  const shortURL = generateRandomString();
-  const longURL = req.body.longURL;
-  
-  urlDatabase[shortURL] = {
-    longURL,
-    userID: req.session.user_id
-  };
-
-  res.redirect(`/urls/${shortURL}`);
 });
 
 //to reach a specific URL page
@@ -94,98 +109,106 @@ app.get("/urls/:shortURL", (req, res) => {
     user: users[req.session.user_id]
   };
 
+  //if the shortURL is not in the database, send an error message
   if (!urlDatabase[req.params.shortURL]) {
-    res.status(404).send("This URL is not associated with any page. Please make sure you have typed it in correctly!");
-    return;
+    res.status(404);
   }
 
+  //render the urls_show HTML page with the help of templateVars
   res.render("urls_show", templateVars);
 });
 
 //editing longURL for a specific shortURL
 app.post("/urls/:shortURL", (req, res) => {
-  console.log(req.body); //to check what is in reg body- hopefully longURL
-  
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
   const user = users[req.session.user_id];
 
+  //prevent individuals who are not logged in to edit the URL
   if (!user) {
     res.status(401).send("Cannot edit the URL when not logged in!");
-    return;
-  }
 
-  if (user["id"] !== urlDatabase[shortURL]["userID"]) {
+  //prevent users who are not the URL's owner to edit the URL
+  } else if (user["id"] !== urlDatabase[shortURL]["userID"]) {
     res.status(403).send('Unauthorized to edit URL');
-    return;
-  }
 
-  urlDatabase[shortURL]['longURL'] = longURL;
-  res.redirect('/urls');
+  //if the two conditions are not met, then the person accessing the URL is the owner and can edit the URL
+  } else {
+    urlDatabase[shortURL]['longURL'] = longURL;
+    res.redirect('/urls');
+  }
 });
 
 //redirect shortURL to longURL site
 app.get("/u/:shortURL", (req, res) => {
   const shortUrlInfo = urlDatabase[req.params.shortURL];
 
+  /*if the shortURL is not in the database,
+    send the user an error page, outside of the tinyapp website*/
   if (!urlDatabase[req.params.shortURL]) {
-    res.status(404).send("This URL is not associated with any page. Please make sure you have typed it in correctly!");
-    return;
+    res.status(404).send("<h2>This URL is not associated with any site. Please make sure you have typed it in correctly!</h2>");
+  
+  //otherwise, redirect to the site associated with the shortURL
+  } else {
+    res.redirect(shortUrlInfo['longURL']);
   }
-
-  res.redirect(shortUrlInfo['longURL']);
 });
 
 //delete a shortURL
 app.post("/urls/:shortURL/delete", (req, res) => {
   const user = users[req.session.user_id];
 
+  //if the individual is not logged in, prevents them from deleting a URL
   if (!user) {
     res.status(401).send('Unauthorized to delete URL.');
-    return;
-  }
-
-  if (user["id"] !== urlDatabase[req.params.shortURL]["userID"]) {
+  
+  //if the id of the user logged in is not the same as the id associated with the URL, prevent them from deleting
+  } else if (user["id"] !== urlDatabase[req.params.shortURL]["userID"]) {
     res.status(403).send('Unauthorized to delete URL.');
-    return;
+  
+  // if both the checks above pass, allow the deletion of the URL
+  } else {
+    delete urlDatabase[req.params.shortURL];
+    //redirect to user's main URL page
+    res.redirect("/urls");
   }
-
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
 });
 
 //LOGIN
 //login page
 app.get('/login', (req, res) => {
   const templateVars = { user: users[req.session.user_id] };
+
+  //redirect a user that is logged in to their main URLs page
   if (users[req.session.user_id]) {
     res.redirect('/urls');
-    return;
+  //otherwise, load up the login page
+  } else {
+    res.render('login', templateVars);
   }
-
-  res.render('login', templateVars);
 });
 
 //login to user, set cookie for user
 app.post("/login", (req, res) => {
   const loginUserEmail = req.body.email;
   const loginUserPassword = req.body.password;
+  //checks for a user in the database using their email
   const checkForUser = getUserByEmail(loginUserEmail, users);
-
+  
+  //if user can't be found from the email inputted, return error message
   if (!checkForUser.email) {
-    res.status(403).send('Unregistered email. Please register this email.');
-    return;
+    res.status(403).send('<h2>Unregistered email. Please register this email.</h2>');
+  
+  //if the password inputted does not correspond to the email in the database, return error message
+  } else if (!bcrypt.compareSync(loginUserPassword, checkForUser.password)) {
+    console.log(bcrypt.compareSync(loginUserPassword, checkForUser.password));
+    res.status(403).send('<h2>Password is incorrect. Please carefully re-enter your password.</h2>');
+  
+  //if both checks pass, then set user to an encrypted cookie and redirect them to their main URL page
+  } else {
+    req.session['user_id'] = checkForUser.id;
+    res.redirect('/urls');
   }
-
-  console.log(bcrypt.compareSync(loginUserPassword, checkForUser.password));
-  if (!bcrypt.compareSync(loginUserPassword, checkForUser.password)) {
-    res.status(403).send('Password is incorrect. Please carefully re-enter your password.');
-    return;
-  }
-
-  //res.cookie('user_id', checkForUser.id);
-  req.session['user_id'] = checkForUser.id;
-  res.redirect('/urls');
 });
 
 //logout of user, clear user cookie info
@@ -198,12 +221,12 @@ app.post("/logout", (req, res) => {
 //register page
 app.get('/register', (req, res) => {
   const templateVars = { user: users[req.session.user_id] };
+  //if user is logged in and goes to the register page, redirect to their main URL page
   if (users[req.session.user_id]) {
     res.redirect('/urls');
-    return;
+  } else {
+    res.render('register', templateVars);
   }
-
-  res.render('register', templateVars);
 });
 
 //register a new user, setting user cookie
@@ -211,30 +234,31 @@ app.post('/register', (req, res) => {
   const id = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
+  //encrypt password with bcrypt
   const hashedPassword = bcrypt.hashSync(password, 10);
+  //returns user info using their email
   const checkForUser = getUserByEmail(email, users);
 
+  //if email or password is left blank when submitting, return error
   if (email === "" || password === "") {
-    res.status(400).send('Invalid email or password.');
-    return;
-  }
-
-  if (checkForUser.email === email) {
-    res.status(400).send('Email is already registered. Please use a different email to register.');
-    return;
-  }
-
-  users[id] = {
-    id,
-    email,
-    password: hashedPassword
-  };
-
-  console.log(users); //check to see if user is added to list
+    res.status(400).send('<h2>Invalid email or password.</h2>');
   
-  //res.cookie('user_id', id);
-  req.session['user_id'] = id;
-  res.redirect('/urls');
+  //if email is already in the database, return an error
+  } else if (checkForUser.email === email) {
+    res.status(400).send('<h2>Email is already registered. Please use a different email to register.</h2>');
+  
+  //if both these checks pass, register the user info, log user in with an encrypted cookie, and redirect to their main URL page
+  } else {
+    users[id] = {
+      id,
+      email,
+      password: hashedPassword
+    };
+
+    console.log(users); //check to see if user is added to list
+    req.session['user_id'] = id;
+    res.redirect('/urls');
+  }
 });
 
 //server listening port
